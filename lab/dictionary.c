9186e14e59df3256eaef1232732d1305bd185619
于欣淼 <>
2020-09-14 08:36:18 +0800

    lab1 pre

---
+++ lab/dictionary.c
@@ -0,0 +1,60 @@
+#include "dictionary.h"
+
+// define your only data structure here, you may define dictionary, elements, etc...
+void *initializer()
+{
+    // your code here
+}
+
+void *search(void *dictionary, int key)
+{
+    // your code here
+}
+
+void insert(void *dictionary, int key, int value)
+{
+    // your code here
+}
+
+void delete(void *dictionary, int key)
+{
+    // your code here
+}
+
+void *minimum(void *dictionary)
+{
+    // your code here
+}
+
+void *maximum(void *dictionary)
+{
+    // your code here
+}
+
+void *predecessor(void *dictionary, int key)
+{
+    // your code here
+}
+
+void *successor(void *dictionary, int key)
+{
+    // your code here
+}
+
+
+int getkey(void *element)
+{
+    // your code here
+}
+
+int getvalue(void *element)
+{
+    // your code here
+}
+
+void free_dict(void *dictionary)
+{
+    // your code here
+}
+
+
