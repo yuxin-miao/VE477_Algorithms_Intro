fc2704e5f2dff8c9de9ced70f9a44b0632409335
于欣淼 <>
2020-09-14 19:20:32 +0800

    lab1.2 hash table with chaining

---
+++ lab/hash.c
@@ -0,0 +1,33 @@
+#include "hash.h"
+// define your only data structure here, you may define hashtable, buckets, elements, etc...
+void *initializer(int size)
+{
+    // your code here
+}
+
+void insert(void* hashtable, int size, int key, int value)
+{
+    // your code here
+}
+
+void delete(void* hashtable, int size, int key)
+{
+    // your code here
+}
+
+void* search(void* hashtable, int size, int key)
+{
+    // your code here
+}
+
+int getValue(void* element)
+{
+    // your code here
+}
+
+void freeHashtable(void* hashtable, int size)
+{
+    // your code here
+}
+
+
